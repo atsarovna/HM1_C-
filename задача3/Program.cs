672edffc99daf1de62851d8fa3d25c012a2b94ed
Jane <evgenya.mun@yandex.ru>
2022-04-15 13:57:57 +0300

    add decision for 3 task

---
+++ задача3/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+if (a%2 == 0) //%2 - остаток от деления на 2
+{
+    Console.WriteLine("Да");
+}
+else 
+{
+    Console.WriteLine("Нет");
+}
+//является ли число четным
