076f6f55553b3d8eb68f809fa0749193901b9a61
Jane <evgenya.mun@yandex.ru>
2022-04-15 13:15:59 +0300

    add decision for 1 task

--- задача1/Program.cs
+++ задача1/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите первое число :");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе число :");
+int b = Convert.ToInt32(Console.ReadLine());
+
+if (a > b)
+{
+    Console.WriteLine("Максимальное число: " + a);
+}
+else 
+{
+    Console.WriteLine("Максимальное число: " + b);
+}
