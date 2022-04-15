88eb756c4b5d72ed2690e5a154d19ba51227e84d
Jane <evgenya.mun@yandex.ru>
2022-04-15 14:59:02 +0300

    add decision for task 4

--- задача4/Program.cs
+++ задача4/Program.cs
@@ -1,13 +1,10 @@
 ﻿Console.Write("Введите число: ");
 int b = Convert.ToInt32(Console.ReadLine());//конечное значение
-int a = -b; //начальное значение
+int a = 2; //начальное значение,потому что четные
 
 while (a <= b)
 {
-    Console.Write(a);
-    a++;
-if (a <= b)
-{
-    Console.Write(", ");
+    Console.Write(a + ", ");
+    a+=2; //+2,тк четные
 }
-}
+//программа показывает все четные от 1 до N
