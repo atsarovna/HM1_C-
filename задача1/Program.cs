c0ee3bf54a2833ad660aaff863434aaf2f131841
Jane <evgenya.mun@yandex.ru>
2022-04-15 14:15:12 +0300

    add changes for all tasks

--- задача1/Program.cs
+++ задача1/Program.cs
@@ -1,7 +1,7 @@
-﻿Console.WriteLine("Введите первое число :");
+﻿Console.Write("Введите первое число :");
 int a = Convert.ToInt32(Console.ReadLine());
 
-Console.WriteLine("Введите второе число :");
+Console.Write("Введите второе число :");
 int b = Convert.ToInt32(Console.ReadLine());
 
 //находим максимальное число
--- задача4/Program.cs
+++ задача4/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Введите число: ");
+int b = Convert.ToInt32(Console.ReadLine());//конечное значение
+int a = -b; //начальное значение
+
+while (a <= b)
+{
+    Console.Write(a);
+    a++;
+if (a <= b)
+{
+    Console.Write(", ");
+}
+}
