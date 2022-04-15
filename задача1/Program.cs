2b2ea2a6ade956eeb7850904e55d432628bd478c
Jane <evgenya.mun@yandex.ru>
2022-04-15 13:19:00 +0300

    corrections for task 1

--- задача1/Program.cs
+++ задача1/Program.cs
@@ -7,8 +7,12 @@ int b = Convert.ToInt32(Console.ReadLine());
 if (a > b)
 {
     Console.WriteLine("Максимальное число: " + a);
+    Console.WriteLine("Минимальное число: " + b);
 }
 else 
 {
     Console.WriteLine("Максимальное число: " + b);
+    Console.WriteLine("Минимальное число: " + a);
 }
+
+// программа,которая выдает какое из 2 чисел большее и меньшее
